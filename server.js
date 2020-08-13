8850d81b924c72aa54ff51e1819cb93efdc83c56
iamnikhilnaik <nnaikme@gmail.com>
2020-08-13 21:39:33 +0530

    adding backend server

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image')
+
+
+
+ 
+  const db = knex({
+    client: 'pg',
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: false,
+    }
+});
+
+
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(cors())
+
+
+
+app.get('/', (req, res) => {
+  res.send('ON');
+});
+
+
+app.post('/signin',signin.handleSignin( db, bcrypt))
+
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id',(req, res) => { profile.handleProfileGet(req, res, db)})
+
+app.post('/imageurl', (req, res) => {image.handleApiCall(req, res,)})
+
+
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Listening on port ${process.env.port}` );
+});
